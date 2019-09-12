d82385aa0cf45302b4d3ab118800bc62ffaaf912
Brage <Brage@getacademy.no>
2019-09-12 14:29:53 +0200

    32

---
+++ WebApplication3/vueclient/src/components/Table.vue
@@ -0,0 +1,29 @@
+<template>   
+   <div class="content" id="table">
+   </div> 
+</template>
+<script>
+import axios from 'axios'
+function test()
+{
+    axios.get('/api/people').then(function (response)
+    {
+        test2(response.data);
+    });
+}
+function test2(x)
+{
+    var arraylength = x.length;
+    for(var i = 0; i < arraylength; i++)
+    {
+        document.getElementById('table').innerHTML += '<div> Navn: ' + x[i].firstName + ' </div>';
+    }
+}
+test();
+export default {
+  name: 'Table',
+  data () {
+    return {    }
+  }
+}
+</script>
